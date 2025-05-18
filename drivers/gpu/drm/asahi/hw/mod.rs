herrnst/linux-asahi

drm/asahi: Expose firmware version to userspace
Might be useful for something.

Signed-off-by: Asahi Lina <lina@asahilina.net>

drivers/gpu/drm/asahi/hw/mod.rs
@@ -321,6 +321,8 @@ pub(crate) struct DynConfig {
     pub(crate) id: GpuIdConfig,
     /// Power calibration configuration for this specific chip/device.
     pub(crate) pwr: PwrConfig,
+    /// Firmware version.
+    pub(crate) firmware_version: Vec<u32>,
 }
 
 /// Specific GPU ID configuration fetched from SGX MMIO registers.

19h/linax

drivers/gpu/drm/asahi/hw/mod.rs
@@ -319,6 +319,8 @@ pub(crate) struct DynConfig {
     pub(crate) id: GpuIdConfig,
     /// Power calibration configuration for this specific chip/device.
     pub(crate) pwr: PwrConfig,
+    /// Firmware version.
+    pub(crate) firmware_version: KVec<u32>,
 }
 
 /// Specific GPU ID configuration fetched from SGX MMIO registers.